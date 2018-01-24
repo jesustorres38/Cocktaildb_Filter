86905c9112391eb22c6c6d9cf1dae503df44d85d
Jesus Torres <jesustorres38@hotmail.com>
2018-01-24 21:07:11 +0100

    https added

--- src/app/services/cocktaildb.service.ts
+++ src/app/services/cocktaildb.service.ts
@@ -13,28 +13,28 @@ export class CocktaildbService {
 
   // GET ALL THE CATEGORIES
   fetchCategories(){
-    return this.http.get('http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
     .map(res => res.json());
   }
 
 
   // GET DRINKS BY ONE CATEGORY
   fetchByCategory(category){
-    return this.http.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c='+category)
+    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c='+category)
     .map(res => res.json());
   }
 
 
   // GET DRINKS BY INGREDIETNS
   fetchByIngredient(ingredient){
-    return this.http.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+ingredient)
+    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i='+ingredient)
     .map(res => res.json()); 
   }
 
 
   // GET DRINK BY ID
   fetchById(id){
-    return this.http.get('http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id)
+    return this.http.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='+id)
     .map(res => res.json()); 
   }
 
